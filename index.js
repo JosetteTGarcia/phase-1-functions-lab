bba87b1b24b5b94ddb1570b56ae83b3c0c84e9b0
Josette Garcia <josettetgarcia@gmail.com>
2022-02-01 16:04:13 -0600

    commit tree 199b958e0d4924fb116d7e9f32a0ba71a73680ea

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+const headQ= 42;
+
+const distanceFromHqInBlocks = function (num2){
+    return Math.abs(headQ - num2);
+}
+
+
+function distanceFromHqInFeet(num) {
+    return distanceFromHqInBlocks(num) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    let calculatesFarePrice;
+    let calculatingFeet= distanceTravelledInFeet(start, destination);
+   if (calculatingFeet < 400 ){
+        return calculatesFarePrice = 0;
+   } else if (calculatingFeet > 400 && calculatingFeet < 2000){
+       calculatesFarePrice = calculatingFeet - 400;
+       return (calculatesFarePrice * 0.02);
+   }
+   else if (calculatingFeet > 2000 && calculatingFeet < 2499){
+       calculatesFarePrice = 25;
+       return calculatesFarePrice;
+   }
+   else if (calculatingFeet > 2500){
+       return 'cannot travel that far';
+   }
+
+}
